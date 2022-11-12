e694d74a4f2fc80a242ffe8db8bfdf47a6c05a6c
fraank <frank.cieslik@gmail.com>
2022-11-12 21:06:09 +0000

    Deploying to gh-pages from @ saunaworlds/wellness-icons.saunaworlds.com@413198852f43d27efda543eaf2b7adb8737303a7 🚀

--- _next/static/nykm2sSLUbXfQVwxvj9_B/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/477-b99b821ccab7bbbf.js","static/chunks/pages/index-5b3547790ab42b22.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
